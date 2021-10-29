e3efdcb0db1008e8491d5a3d45a6ac4ef0a596d7
Pedro Penha <pedro.penha.martins@gmail.com>
2021-10-29 01:29:55 -0300

    arrumando js

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,4 @@
-let notes = window.localStorage.getItem('notes') || '{"data": []}';
-notes = JSON.parse(notes);
-
-let updateList = function () {
-    console.log('[Application] start watch');
-
-    Array.observe(notes.data, function (changes) {
-        let index = null;
-        let value = '';
-        let status = null;
-
-        if (changes[0].type === 'splice') {
-            index = changes[0].index;
-            value = changes[0].object[index];
-            status = (changes[0].addedCount > 0) ? 'created' : 'removed';
-        }
-
-        if (changes[0].type === 'update') {
-            index = changes[0].name;
-            value = changes[0].object[index];
-            status = 'updated';
-        }
-
-        if (!value && status === 'created' && status === 'updated') {
-            return;
-        }
-
-        let notesTag = document.getElementById('notes');
-
-        if (status === 'updated') {
-            console.log('implementar');
-        }
-
-        if (status === 'removed') {
-            let listOfNotes = document.querySelectorAll('#notes li');
-            notesTag.removeChild(listOfNotes[index]);
-        }
-
-        if (status === 'created') {
-            let newLi = document.createElement('li');
-            newLi.innerHTML = value;
-            notesTag.appendChild(newLi);
-        }
-
-        window.localStorage.setItem('notes', JSON.stringify(notes));
-    });
-}
-
-let createNote = function () {
+function calcular() {
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
     let result = 0.00;
@@ -59,46 +11,12 @@ let createNote = function () {
         document.getElementById('err').innerHTML = "Peso e altura não podem ser vazios";
     else{
         result = parseFloat(peso) / (parseFloat(altura)*parseFloat(altura));
-        notes.data.push(result);
     }
 
     peso = '';
     altura = '';
-};
-
-updateList();
-
-document.addEventListener('DOMContentLoaded', function (event) {
-    let listOfNotes = document.getElementById('notes');
-    let listHtml = '';
-
-    for(let i=0; i< notes.data.length; i++) {
-        listHtml += '<li>' + notes.data[i] +  '</li>';
-    }
-
-    listOfNotes.innerHTML = listHtml;
-
-    let formAddNotes = document.getElementById('form-add-note')
-    formAddNotes.addEventListener('submit', function (e) {
-        e.preventDefault();
-        createNote();
-    });
-});
-
-document.addEventListener('click', function (e) {
-    let notesTag = document.getElementById('notes');
-
-    if (e.target.parentElement === notesTag) {
-        if (confirm('remover esta nota?')) {
-            let listOfNotes = document.querySelectorAll('#notes li');
-            listOfNotes.forEach(function (item, index) {
-                if (e.target === item) {
-                    notes.data.splice(index, 1);
-                }
-            });
-        }
-    }
-});
+    console.log(result)
+}
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
